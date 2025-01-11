e32f4e3f2ec3fb6a400595927e71679eafdf4695
Dano <danocolombo@gmail.com>
2025-01-11 14:49:40 -0500

    base

---
+++ app/ui/fonts.ts
@@ -0,0 +1,12 @@
+import { Inter, Roboto, Lusitana } from 'next/font/google';
+
+export const inter = Inter({ subsets: ['latin'] });
+export const roboto = Roboto({
+    subsets: ['latin'],
+    weight: '500',
+});
+
+export const lusitana = Lusitana({
+    subsets: ['latin'],
+    weight: '700',
+});
